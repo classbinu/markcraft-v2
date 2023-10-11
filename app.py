231ab8fdcefb567b263bde3ed3bbbdf9e804c6b7
Classbinu <classbinu@gmail.com>
2023-10-11 20:16:10 +0900

    프론트엔드 서버 렌더링 병합 중

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ app.register_blueprint(pages_bp)
 app.register_blueprint(api_bp)
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=4999, debug=True)
--- routes/pages.py
+++ routes/pages.py
@@ -5,24 +5,28 @@ pages_bp = Blueprint('pages',__name__)
 
 @pages_bp.route('/', methods=['GET'])
 def get_home():
-    return 'This is home!'
+    return render_template('index.html')
 
-@pages_bp.route('/page/signin', methods=['GET'])
+@pages_bp.route('/signin', methods=['GET'])
 def get_signin():
-    return 'This is signin!'
+    return render_template('auth/signin.html')
 
-@pages_bp.route('/page/signup', methods=['GET'])
+@pages_bp.route('/signup', methods=['GET'])
 def get_signup():
-    return 'This is signup!'
+    return render_template('auth/signup.html')
 
-@pages_bp.route('/page/lecture', methods=['GET'])
-def get_lecture():
-    return 'This is lecture!'
+@pages_bp.route('/classroom', methods=['GET'])
+def get_classroom():
+    return render_template('classroom/index.html')
 
-@pages_bp.route('/page/note', methods=['GET'])
-def get_note():
-    return 'This is note!'
+@pages_bp.route('/classroom/chapter1', methods=['GET'])
+def get_chapter1():
+    return render_template('classroom/chapter1.html')
 
-@pages_bp.route('/page/timeattack', methods=['GET'])
+@pages_bp.route('/timeattack', methods=['GET'])
 def get_timeattack():
-    return 'This is timeattack!'
+    return render_template('timeattack/index.html')
+
+@pages_bp.route('/note', methods=['GET'])
+def get_note():
+   return render_template('note/index.html')
