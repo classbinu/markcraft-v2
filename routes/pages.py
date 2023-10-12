0ac95bdff3426a669ed3e3d5f525c6d7c1c67fd4
Classbinu <classbinu@gmail.com>
2023-10-12 16:38:04 +0900

    타임어택 오프라인 코드 구현 완료

--- routes/pages.py
+++ routes/pages.py
@@ -1,11 +1,52 @@
 from flask import Flask, Blueprint, render_template, request, redirect, url_for, flash
 from pymongo import MongoClient
 from .api import verify_token
-from random import choice
+from random import choice, sample
 
 pages_bp = Blueprint("pages", __name__)
 client = MongoClient("localhost", 27017)
 db = client.test
+
+study_set = {
+    1: [
+        '# 1수준 헤더입니다', 
+        '## 2수준 헤더입니다', 
+        '### 3수준 헤더입니다',
+        '#### 4수준 헤더입니다',
+        '##### 5수준 헤더입니다',
+        '###### 6수준 헤더입니다'
+    ], 
+    2: [
+        '안녕하세요<br>크래프톤 정글입니다',
+        '이곳은 경기대학교입니다<br>언덕이 무척 높습니다',
+    ],
+    3: [
+        '수평선 위입니다 *** 수평선 아래입니다'
+    ],
+    4: [
+        '**이 문장은 볼드체입니다**',
+        '_이 문장은 이탤릭입니다_',
+        '~~이 문장은 취소선입니다~~',
+    ],
+    5: [
+        '>인용입니다'
+    ],
+    6: [
+        '모듈을 불러오는 예약어는 `import`입니다'
+    ],
+    7: [
+        '+ 순서가 없는 목록입니다'
+    ],
+    8: [
+        '1. 순서가 있는 목록입니다'
+    ],
+    9: [
+        '[Google](https://www.google.com)'
+    ],
+    10: [
+        '![크래프톤 정글](https://buly.kr/DwAhhgk)'
+    ]
+}
 
 
 def millisecondsToMinutesSeconds(milliseconds):
@@ -68,48 +109,6 @@ def get_classroom():
 
 @pages_bp.route("/classroom/chapter/<int:chapter_id>", methods=["GET"])
 def get_chapter(chapter_id):
-
-    study_set = {
-        1: [
-            '# 1수준 헤더입니다', 
-            '## 2수준 헤더입니다', 
-            '### 3수준 헤더입니다',
-            '#### 4수준 헤더입니다',
-            '##### 5수준 헤더입니다',
-            '###### 6수준 헤더입니다'
-        ], 
-        2: [
-            '안녕하세요<br>크래프톤 정글입니다',
-            '이곳은 경기대학교입니다<br>언덕이 무척 높습니다',
-        ],
-        3: [
-            '수평선 위입니다 *** 수평선 아래입니다'
-        ],
-        4: [
-            '**이 문장은 볼드체입니다**',
-            '_이 문장은 이탤릭입니다_',
-            '~~이 문장은 취소선입니다~~',
-        ],
-        5: [
-            '>인용입니다'
-        ],
-        6: [
-            '모듈을 불러오는 예약어는 `import`입니다'
-        ],
-        7: [
-            '+ 순서가 없는 목록입니다'
-        ],
-        8: [
-            '1. 순서가 있는 목록입니다'
-        ],
-        9: [
-            '[Google](https://www.google.com)'
-        ],
-        10: [
-            '![크래프톤 정글](https://buly.kr/DwAhhgk)'
-        ]
-    }
-
     question = choice(study_set[chapter_id])
     template_path = f'classroom/chapter{chapter_id}.html'
     return render_template(template_path, question=question)
@@ -125,18 +124,35 @@ def get_timeattack():
         users = verify_token(token)
         user = db.users.find_one({"email": users["email"]})
         myBestTime = int(user["bestTime"])
-        print(myBestTime)
         formattedMyBestTime = millisecondsToMinutesSeconds(myBestTime)
 
         topRanker = list(db.users.find().sort("bestTime", 1).limit(1))
         formattedBestTime = millisecondsToMinutesSeconds(topRanker[0]["bestTime"])
+
+        test_set = ['# 헤더1', '## 헤더2', '### 헤더3', '#### 헤더4', '##### 헤더5']
+        questions = sample(test_set, 5)
+
+        # 랭커 가져오기
+        rankers = list(db.users.find().sort("bestTime", 1).limit(3))
+        # rankers에 있는 회원 수가 3명 미만인 경우 더미 데이터 추가
+        while len(rankers) < 3:
+            rankers.append({"nickname": "참가자 없음", "bestTime": 5940000})
+
+        for ranker in rankers:
+            ranker["bestTime"] = millisecondsToMinutesSeconds(ranker["bestTime"])
+
         return render_template(
             "timeattack/index.html",
             topRanker=topRanker,
             formattedBestTime=formattedBestTime,
             formattedMyBestTime=formattedMyBestTime,
+            questions = questions,
+            ranker_1=rankers[0],
+            ranker_2=rankers[1],
+            ranker_3=rankers[2],
         )
-    except:
+    except Exception as e:
+        print(e)
         return redirect(url_for("pages.get_signin"))
 
 
